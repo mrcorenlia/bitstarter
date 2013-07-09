144eaaa21fb16e0aa0644208e94a8a35aa5d39f1
Chris Power <cpower@gmail.com>
2013-07-09 22:53:09 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+function getIndex(){
+  var str = fs.readFileSync('index.html').toString();
+  return str;
+};
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+      response.send(getIndex());
 });
 
 var port = process.env.PORT || 5000;
